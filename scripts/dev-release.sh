e762e4f25e4efb79e63f139b9e8a9567036dbe6d
Brady Lill <bradylill@gmail.com>
2018-08-22 15:44:03 -0600

    Publish dev version of orbs on master.

--- scripts/dev-release.sh
+++ scripts/dev-release.sh
@@ -8,5 +8,9 @@ UPDATED_ORBS=$(git diff $COMMIT_RANGE --name-status | \
                grep -vE '^[A-Z!?]{1,2}\s+')
 
 for ORB in $UPDATED_ORBS; do
-  circleci orb publish release src/$ORB/orb.yml circleci $ORB dev:snapshot
+  if [[ -z "$CIRCLECI_API_TOKEN" ]]; then
+    circleci orb publish release src/$ORB/orb.yml circleci $ORB dev:snapshot
+  else
+    circleci orb publish release src/$ORB/orb.yml circleci $ORB dev:snapshot --token $CIRCLECI_API_TOKEN
+  fi
 done
