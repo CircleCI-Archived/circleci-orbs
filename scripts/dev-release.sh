98d9e0fac951aeaf77d07c3ae7cc6d81a38374e5
ndintenfass <nathan@dintenfass.com>
2018-09-24 01:18:55 -0700

    Change config to publish dev orbs on the branch name and move to 2.1 config

--- scripts/dev-release.sh
+++ scripts/dev-release.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -eo pipefail
 
-COMMIT_RANGE=$(echo $CIRCLE_COMPARE_URL | sed 's:^.*/compare/::g')
-echo "Commit range: " $COMMIT_RANGE
+# COMMIT_RANGE=$(echo $CIRCLE_COMPARE_URL | sed 's:^.*/compare/::g')
+# echo "Commit range: " $COMMIT_RANGE
 
-UPDATED_ORBS=$(git diff $COMMIT_RANGE --name-status | \
-               sed -E 's/.*src\/(.+)\/.*/\1/' | \
-               grep -vE '^[A-Z!?]{1,2}\s+')
+# UPDATED_ORBS=$(git diff $COMMIT_RANGE --name-status | \
+#                sed -E 's/.*src\/(.+)\/.*/\1/' | \
+#                grep -vE '^[A-Z!?]{1,2}\s+')
 
-for ORB in $UPDATED_ORBS; do
+# for ORB in $UPDATED_ORBS; do
+for ORB in src/*; do
   if [[ -z "$CIRCLECI_API_TOKEN" ]]; then
-    circleci orb publish release src/$ORB/orb.yml circleci $ORB dev:snapshot
+    circleci circleci orb publish dev src/$ORB/orb.yml circleci $ORB dev:${CIRCLE_BRANCH}
   else
-    circleci orb publish release src/$ORB/orb.yml circleci $ORB dev:snapshot --token $CIRCLECI_API_TOKEN
+    circleci circleci orb publish dev src/$ORB/orb.yml circleci $ORB dev:${CIRCLE_BRANCH} --token $CIRCLECI_API_TOKEN
   fi
 done
