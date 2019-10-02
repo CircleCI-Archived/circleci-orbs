a18bb2e642dd147fee5db48dc3dcf018f6436547
Julz4455 <julius.nieves@sascholar.org>
2019-10-02 09:03:24 -0400

    Compress validate-orbs.sh
    Compressing validate-orbs.sh to save file space and execution time.

--- scripts/validate-orbs.sh
+++ scripts/validate-orbs.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -eo pipefail
-
 for ORB in src/*; do
   echo "Validating $ORB ..."
-
   circleci orb validate $ORB/orb.yml; RETURN_CODE=$?
-
-  if [[ $RETURN_CODE != 0 ]]; then
-  	exit 1
-  fi
+  if [[ $RETURN_CODE -ne 0 ]]; then exit 1; fi
 done
