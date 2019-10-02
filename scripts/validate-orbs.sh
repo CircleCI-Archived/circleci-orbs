1e0d119d7d441e2003e05002bc513796a0a5dbbe
Harsh2803 <49323194+Harsh2803@users.noreply.github.com>
2019-10-02 12:28:49 +0530

    modified program style and added comments

--- scripts/validate-orbs.sh
+++ scripts/validate-orbs.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -eo pipefail
-
-for ORB in src/*; do
+#for statement start
+for ORB in src/*; 
+ do
   echo "Validating $ORB ..."
 
-  circleci orb validate $ORB/orb.yml; RETURN_CODE=$?
-
-  if [[ $RETURN_CODE != 0 ]]; then
-  	exit 1
+  circleci orb validate $ORB/orb.yml; 
+  RETURN_CODE=$?
+#if statement start
+  if [[ $RETURN_CODE != 0 ]]; 
+  then
+  	exit 1 #exits the loop
   fi
 done
+#finish
